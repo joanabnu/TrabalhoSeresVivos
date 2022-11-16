9c27d231b5005d7dd7534bc04c57d7bfd67fa78b
Joana Maria <joanamariasa2611@gmail.com>
2022-11-16 17:41:47 -0300

    Adicionando caracteristicas de ser vivo

--- src/main/java/trabalhogrupodevs2blu/SerVivo.java
+++ src/main/java/trabalhogrupodevs2blu/SerVivo.java
@@ -1,5 +1,139 @@
 package trabalhogrupodevs2blu;
 
 public class SerVivo {
-    
+    private double peso;
+    private double altura;
+    private int idade;
+    private boolean multiCelular;
+    private boolean possuiDNA;
+    private boolean possuiRNA;
+    private boolean possuiOrganelas;
+    private boolean eucariontes;
+    private boolean procariontes;
+
+    /**
+     * @return the peso
+     */
+    public double getPeso() {
+        return peso;
+    }
+
+    /**
+     * @param peso the peso to set
+     */
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    /**
+     * @return the altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * @param altura the altura to set
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * @return the idade
+     */
+    public int getIdade() {
+        return idade;
+    }
+
+    /**
+     * @param idade the idade to set
+     */
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    /**
+     * @return the multiCelular
+     */
+    public boolean isMultiCelular() {
+        return multiCelular;
+    }
+
+    /**
+     * @param multiCelular the multiCelular to set
+     */
+    public void setMultiCelular(boolean multiCelular) {
+        this.multiCelular = multiCelular;
+    }
+
+    /**
+     * @return the possuiDNA
+     */
+    public boolean isPossuiDNA() {
+        return possuiDNA;
+    }
+
+    /**
+     * @param possuiDNA the possuiDNA to set
+     */
+    public void setPossuiDNA(boolean possuiDNA) {
+        this.possuiDNA = possuiDNA;
+    }
+
+    /**
+     * @return the possuiRNA
+     */
+    public boolean isPossuiRNA() {
+        return possuiRNA;
+    }
+
+    /**
+     * @param possuiRNA the possuiRNA to set
+     */
+    public void setPossuiRNA(boolean possuiRNA) {
+        this.possuiRNA = possuiRNA;
+    }
+
+    /**
+     * @return the possuiOrganelas
+     */
+    public boolean isPossuiOrganelas() {
+        return possuiOrganelas;
+    }
+
+    /**
+     * @param possuiOrganelas the possuiOrganelas to set
+     */
+    public void setPossuiOrganelas(boolean possuiOrganelas) {
+        this.possuiOrganelas = possuiOrganelas;
+    }
+
+    /**
+     * @return the eucariontes
+     */
+    public boolean isEucariontes() {
+        return eucariontes;
+    }
+
+    /**
+     * @param eucariontes the eucariontes to set
+     */
+    public void setEucariontes(boolean eucariontes) {
+        this.eucariontes = eucariontes;
+    }
+
+    /**
+     * @return the procariontes
+     */
+    public boolean isProcariontes() {
+        return procariontes;
+    }
+
+    /**
+     * @param procariontes the procariontes to set
+     */
+    public void setProcariontes(boolean procariontes) {
+        this.procariontes = procariontes;
+    }
 }
