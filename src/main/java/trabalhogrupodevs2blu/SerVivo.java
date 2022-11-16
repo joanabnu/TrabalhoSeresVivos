f8fed446897bcf1bc789a9d9910d6776a843af7a
Joana Maria <joanamariasa2611@gmail.com>
2022-11-16 17:32:37 -0300

    Inicio

---
+++ src/main/java/trabalhogrupodevs2blu/SerVivo.java
@@ -0,0 +1,5 @@
+package trabalhogrupodevs2blu;
+
+public class SerVivo {
+    
+}
---
+++ src/main/java/trabalhogrupodevs2blu/main.java
@@ -0,0 +1,7 @@
+package trabalhogrupodevs2blu;
+
+public class main {
+  public static void main(String[] args){
+      System.out.println("Inicio");
+  } 
+}
