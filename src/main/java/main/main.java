7807264617d5ae4d9eb7cd6b33e87d9608024c75
Caio Neves <nevescaioneves@gmail.com>
2022-11-21 10:51:37 -0300

    implementacoes finais

--- src/main/java/main/main.java
+++ src/main/java/main/main.java
@@ -2,11 +2,6 @@ package main;
 
 import java.util.Scanner;
 
-import animalia.EspecieAnimalia;
-import plantae.EspeciePlantae;
-import monera.EspecieMonera;
-import protista.EspecieProtista;
-
 public class main {
   public static void main(String[] args) {
     
@@ -47,30 +42,36 @@ public class main {
     opcao = leia.next();
     
     if(opcao.equalsIgnoreCase("sim")) {
-    	System.out.println("Selecione o reino que você deseja listar: ");
-    	System.out.println("\n 1 - Animal \n 2- Planta \n 3 - Monera \n 4 - Protista \n 5 - Fungi:");
-    	int opcao2 = leia.nextInt();
-    	
-    	switch (opcao2) {
-	      case 1:
-	    	serVivo.mostrarDados("1");
-	        break;
-	      case 2:
-	    	serVivo.mostrarDados("2");
-	    	break;
-	      case 3:
-	        serVivo.mostrarDados("3");
-	        break;
-	      case 4:
-	        serVivo.mostrarDados("4");
-	        break;
-	      case 5:
-		    serVivo.mostrarDados("5");
-		    break;
-	      default:
-	        System.out.println("Opção inválida");
-	        break;
-	    }
+    	do {
+    		System.out.println("Selecione o reino que você deseja listar: ");
+    		System.out.println("\n 1 - Animal \n 2- Planta \n 3 - Monera \n 4 - Protista \n 5 - Fungi:");
+    		int opcao2 = leia.nextInt();
+    		
+    		switch (opcao2) {
+    		case 1:
+    			serVivo.mostrarDados("1");
+    			break;
+    		case 2:
+    			serVivo.mostrarDados("2");
+    			break;
+    		case 3:
+    			serVivo.mostrarDados("3");
+    			break;
+    		case 4:
+    			serVivo.mostrarDados("4");
+    			break;
+    		case 5:
+    			serVivo.mostrarDados("5");
+    			break;
+    		default:
+    			System.out.println("Opção inválida");
+    			break;
+    		}
+    		
+    		System.out.println("\n Deseja continuar? ");
+    	    opcao = leia.next();
+    	}while(opcao.equalsIgnoreCase("sim"));
     }
+    leia.close();
   }
 }
